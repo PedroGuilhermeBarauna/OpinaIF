07ed7f9f62e7571478c69d35003c8e2647d555f5
Pedro Guilherme Barauna <pedroguiifc@gmail.com>
2021-03-12 19:06:27 -0300

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,124 +1,134 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="../semantic/semantic.css">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="../semantic/semantic.css">
 
-    <style type="text/css">
-      body {
-        background-color:  #00B5AD;
-      }
-      .ui.item {
-        margin-left: 1000px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="ui fixed inverted menu">
-      <div class="ui container">
-        <a class="ui item" href="" class="item">Sair</a>
+  <style type="text/css">
+    body {
+      background-color:  #00B5AD;
+    }
+    .ui.item {
+      margin-left: 1000px;
+    }
+    .ui.description {
+      margin-left: 90px;
+    }
+
+    .ui.icon.button {
+      margin-top: 25px;
+      margin-right: 50px;
+    }
+
+    .ui.img {
+      max-height: 80;
+      max-width: 100px;
+    }
+
+  </style>
+</head>
+<body>
+  <div class="ui fixed inverted menu">
+    <div class="ui container">
+      <a class="ui item" href="" class="item">Sair</a>
+    </div>
+  </div>
+
+  <div class="ui four column doubling stackable grid container">
+    <div class="column">
+        <div class="item">
+          <p> 
+            <img class="ui img"  src="../img/formulario.jpg" align="left">
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
+          </p><br>
+          <a href="">
+            <button class="ui right labeled icon button">
+            <i class="right arrow icon"></i>
+            Responder
+          </button></a>
+      </div>
+
+    </div>
+    <div class="column">
+        <div class="item">
+          <p> 
+            <img class="ui img"  src="../img/formulario.jpg" align="left">
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
+          </p><br>
+          <a href="">
+            <button class="ui right labeled icon button">
+            <i class="right arrow icon"></i>
+            Responder
+          </button></a>
       </div>
     </div>
-
-    <div class="ui four column doubling stackable grid container">
-          <div class="column">
-<div class="ui items">
-  <div class="item">
-    <a class="ui tiny image">
-      <img src="../img/formulario.jpg">
-    </a>
-    <div class="content">
-      <div class="description">
-        <p></p>
+    <div class="column">
+        <div class="item">
+          <p> 
+            <img class="ui img"  src="../img/formulario.jpg" align="left">
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
+          </p><br>
+          <a href="">
+            <button class="ui right labeled icon button">
+            <i class="right arrow icon"></i>
+            Responder
+          </button></a>
+      </div>
+    </div>
+    <div class="column">
+        <div class="item">
+          <p> 
+            <img class="ui img"  src="../img/formulario.jpg" align="left">
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
+          </p><br>
+          <a href="">
+            <button class="ui right labeled icon button">
+            <i class="right arrow icon"></i>
+            Responder
+          </button></a>
       </div>
     </div>
   </div>
 </div>
-            <div class="ui inverted section "></div>
-            <a href=""><button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-            </button></a><img class="wireframe">
-          </div>
-          <div class="column">
-<div class="ui items">
-  <div class="item">
-    <a class="ui tiny image">
-      <img src="../img/formulario.jpg">
-    </a>
-    <div class="content">
-      <div class="description">
-        <p>  </p>
-      </div>
-    </div>
-  </div>
-</div>
-            <div class="ui inverted section "></div>
-            <a href=""><button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-            </button></a><img class="wireframe">
-          </div>
-          <div class="column">
-<div class="ui items">
-  <div class="item">
-    <a class="ui tiny image">
-      <img src="../img/formulario.jpg">
-    </a>
-    <div class="content">
-      <div class="description">
-        <p>  </p>
-      </div>
-    </div>
-  </div>
-</div>
-            <div class="ui inverted section "></div>
-            <a href=""><button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-            </button></a><img class="wireframe">
-          </div>
-    </div>
-    </div>
 
-    </div>
-    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br>
-    <div class="ui inverted vertical footer segment">
-    <div class="ui center aligned container">
+</div>
+<br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br>
+<div class="ui inverted vertical footer segment">
+  <div class="ui center aligned container">
     <div class="ui stackable inverted divided grid">
-    <div class="three wide column">
-    <h4 class="ui inverted header">Pedro Guilherme</h4>
-    <div class="ui inverted link list">
-    <a href="#" class="item">Facebook</a>
-    <a href="#" class="item">Twitter</a>
-    <a href="#" class="item"> github</a>
-    <a href="#" class="item">instagram</a>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Pedro Guilherme</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
+          <a href="#" class="item"> github</a>
+          <a href="#" class="item">instagram</a>
+        </div>
+      </div>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Gabriel Soka</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
+          <a href="#" class="item"> github</a>
+          <a href="#" class="item">instagram</a>
+        </div>
+      </div>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Jefferson</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
+          <a href="#" class="item"> github</a>
+          <a href="#" class="item">instagram</a>
+        </div>
+      </div>
+      <div class="seven wide column">
+        <h4 class="ui inverted header">Desenvolvedores</h4>
+        <p>Visite a pagina dos desenvolvedores e seguia-os</p>
+      </div>
     </div>
-    </div>
-    <div class="three wide column">
-    <h4 class="ui inverted header">Gabriel Soka</h4>
-    <div class="ui inverted link list">
-    <a href="#" class="item">Facebook</a>
-    <a href="#" class="item">Twitter</a>
-    <a href="#" class="item"> github</a>
-    <a href="#" class="item">instagram</a>
-    </div>
-    </div>
-    <div class="three wide column">
-    <h4 class="ui inverted header">Jefferson</h4>
-    <div class="ui inverted link list">
-    <a href="#" class="item">Facebook</a>
-    <a href="#" class="item">Twitter</a>
-    <a href="#" class="item"> github</a>
-    <a href="#" class="item">instagram</a>
-    </div>
-    </div>
-    <div class="seven wide column">
-    <h4 class="ui inverted header">Desenvolvedores</h4>
-    <p>Visite a pagina dos desenvolvedores e seguia-os</p>
-    </div>
-    </div>
-    </div>
-    </div>
-  </body>
+  </div>
+</div>
+</body>
 </html>
