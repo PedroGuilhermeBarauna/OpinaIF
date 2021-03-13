6f406cc4fbb4723c2467aba6c9e64b61faa0d14c
Pedro Guilherme Barauna <pedroguiifc@gmail.com>
2021-03-13 13:49:59 -0300

    Merge branch 'main' of https://github.com/PedroGuilhermeBarauna/OpinaIF into main

--- views/index.php
+++ views/index.php
@@ -6,12 +6,10 @@
 
   <style type="text/css">
     body {
-      background-color:  white;
+      background-color:  #00B5AD;
     }
-    .ui.white.button {
-      margin-left: 1150px;
-      background-color: red;
-      color: white;
+    .ui.item {
+      margin-left: 1000px;
     }
     .ui.description {
       margin-left: 90px;
@@ -20,8 +18,6 @@
     .ui.icon.button {
       margin-top: 25px;
       margin-right: 50px;
-      background-color: green;
-      color: white;
     }
 
     .ui.img {
@@ -29,27 +25,21 @@
       max-width: 100px;
     }
 
-    .ui.item {
-      margin-top: 30px;
-    }
-    .ui.inverted.vertical.footer.segment{
-      position:absolute;
-      bottom:0;
-      width:100%;
-    }
-
   </style>
 </head>
 <body>
-  <div class="ui fixed inverted black menu">
-      <a class="ui white button" href="">Sair</a>
+  <div class="ui fixed inverted menu">
+    <div class="ui container">
+      <a class="ui item" href="" class="item">Sair</a>
+    </div>
   </div>
+
   <div class="ui four column doubling stackable grid container">
     <div class="column">
-        <div class="ui item">
+        <div class="item">
           <p> 
             <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Formulário referente a pesquisa de satisfação do refeitorio.
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
           </p><br>
           <a href="">
             <button class="ui right labeled icon button">
@@ -57,10 +47,13 @@
             Responder
           </button></a>
       </div>
-        <div class="ui item">
+
+    </div>
+    <div class="column">
+        <div class="item">
           <p> 
             <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Qual obra deve ter prioridade no ginasio de esportes ?
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
           </p><br>
           <a href="">
             <button class="ui right labeled icon button">
@@ -70,21 +63,10 @@
       </div>
     </div>
     <div class="column">
-        <div class="ui item">
+        <div class="item">
           <p> 
             <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Pesquisa sobre o que deve ser feito no dia das mães
-          </p><br>
-          <a href="">
-            <button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-          </button></a>
-      </div>
-        <div class="ui item">
-          <p> 
-            <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Relatório de fim de ano sobre desempenho dos alunos por disciplina
+            Nessa parte vem a descrição do formulário que o usuário cadastrou
           </p><br>
           <a href="">
             <button class="ui right labeled icon button">
@@ -94,31 +76,7 @@
       </div>
     </div>
     <div class="column">
-        <div class="ui item">
-          <p> 
-            <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Perguntas sobre o desempenho dos professores
-          </p><br>
-          <a href="">
-            <button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-          </button></a>
-      </div>
-        <div class="ui item">
-          <p> 
-            <img class="ui img"  src="../img/formulario.jpg" align="left">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus.
-          </p><br>
-          <a href="">
-            <button class="ui right labeled icon button">
-            <i class="right arrow icon"></i>
-            Responder
-          </button></a>
-      </div>
-    </div>
-    <div class="column">
-        <div class="ui item">
+        <div class="item">
           <p> 
             <img class="ui img"  src="../img/formulario.jpg" align="left">
             Nessa parte vem a descrição do formulário que o usuário cadastrou
@@ -134,20 +92,40 @@
 </div>
 
 </div>
+<br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br><br><br><br><br><br><br>    <br><br><br><br>
 <div class="ui inverted vertical footer segment">
   <div class="ui center aligned container">
     <div class="ui stackable inverted divided grid">
-      <div class="two wide column">
+      <div class="three wide column">
         <h4 class="ui inverted header">Pedro Guilherme</h4>
         <div class="ui inverted link list">
           <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
           <a href="#" class="item"> github</a>
-          <a href="#" class="item">Site pessoal</a>
+          <a href="#" class="item">instagram</a>
         </div>
       </div>
       <div class="three wide column">
-        <h4 class="ui inverted header">Desenvolvido por</h4>
-        <p>Pedro Guilherme Barauna © Todos os direitos reservados</p>
+        <h4 class="ui inverted header">Gabriel Soka</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
+          <a href="#" class="item"> github</a>
+          <a href="#" class="item">instagram</a>
+        </div>
+      </div>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Jefferson</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Facebook</a>
+          <a href="#" class="item">Twitter</a>
+          <a href="#" class="item"> github</a>
+          <a href="#" class="item">instagram</a>
+        </div>
+      </div>
+      <div class="seven wide column">
+        <h4 class="ui inverted header">Desenvolvedores</h4>
+        <p>Visite a pagina dos desenvolvedores e seguia-os</p>
       </div>
     </div>
   </div>
